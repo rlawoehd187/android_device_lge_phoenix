d230ac1ace4dc060697e6592b5dfa5d72343d76b
hiru <rlawoehd187@naver.com>
2022-11-28 00:08:42 +0900

    phoenix: fix some bugs.
    Signed-off-by: hiru <rlawoehd187@naver.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -56,15 +56,21 @@ ENABLE_SCHEDBOOST := true
 BOARD_PROVIDES_GPTUTILS := true
 
 # Kernel
-BOARD_NAME := Qualcomm
-BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=2048 androidboot.configfs=true androidboot.usbcontroller=a800000.dwc3 loop.max_part=7 androidboot.hardware=phoenix_sprout
-BOARD_KERNEL_BASE        := 0x00000000
-BOARD_KERNEL_OFFSET := 0x00008000
-BOARD_KERNEL_PAGESIZE    := 4096
-BOARD_KERNEL_TAGS_OFFSET := 0x00000100
-BOARD_RAMDISK_OFFSET     := 0x01000000
-BOARD_SECOND_OFFSET := 0x00f00000
-BOARD_MKBOOTIMG_ARGS := --board $(BOARD_NAME) --base $(BOARD_KERNEL_BASE) --pagesize $(BOARD_KERNEL_PAGESIZE) --kernel_offset $(BOARD_KERNEL_OFFSET) --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET) --second_offset $(BOARD_SECOND_OFFSET)
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 user_debug=31
+BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x37 ehci-hcd.park=3
+BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1 sched_enable_hmp=1
+BOARD_KERNEL_CMDLINE += sched_enable_power_aware=1 service_locator.enable=1
+BOARD_KERNEL_CMDLINE += swiotlb=2048 androidboot.configfs=true
+BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a800000.dwc3
+BOARD_KERNEL_CMDLINE += loop.max_part=7
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += androidboot.hardware=phoenix_sprout
+TARGET_KERNEL_ARCH := arm64
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_SEPARATED_DTBO := true
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+
 TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilt/Image.gz-dtb
 TARGET_BOARD_KERNEL_HEADERS := $(LOCAL_PATH)/kernel-headers
 
@@ -73,7 +79,6 @@ TARGET_NO_KERNEL := false
 #
 
 # Partitions
-BOARD_HAS_LARGE_FILESYSTEM := true
 BOARD_FLASH_BLOCK_SIZE := 262144
 
 BOARD_BOOTIMAGE_PARTITION_SIZE := 41943040
@@ -93,53 +98,51 @@ BOARD_USES_RECOVERY_AS_BOOT := true
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
 
 TARGET_USES_MKE2FS := true
-BOARD_ROOT_EXTRA_FOLDERS := firmware persist
-BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+
+# Partitions (listed in the file) to be wiped under recovery.
+TARGET_RECOVERY_WIPE := $(LOCAL_PATH)/recovery/root/etc/recovery.wipe
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery/root/etc/recovery.fstab
 
 # Recovery
-TARGET_NO_KERNEL := false
-RECOVERY_SDCARD_ON_DATA := true
-RECOVERY_NEED_SELINUX_FIX := true
-USE_RECOVERY_INSTALLER := true
-RECOVERY_INSTALLER_PATH := $(LOCAL_PATH)/installer
-TARGET_RECOVERY_PIXEL_FORMAT := RGBA_8888
-TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
-
-# Crypto
-TW_INCLUDE_CRYPTO := true
-TW_INCLUDE_CRYPTO_FBE := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_HAS_NO_SELECT_BUTTON := true
 
 # TWRP specific build flags
-TARGET_RECOVERY_QCOM_RTC_FIX := true
 BOARD_HAS_NO_REAL_SDCARD := true
-BOARD_SUPPRESS_SECURE_ERASE := true
-TW_BRIGHTNESS_PATH := /sys/class/backlight/panel0-backlight/brightness
-TW_CRYPTO_USE_SYSTEM_VOLD := qseecomd
-TW_INCLUDE_FB2PNG := true
-TW_DEFAULT_BRIGHTNESS := 100
-TW_DEFAULT_LANGUAGE := en
-TW_EXTRA_LANGUAGES := true
-TW_EXTERNAL_STORAGE_MOUNT_POINT := external_sd
-TW_EXTERNAL_STORAGE_PATH := /external_sd
+RECOVERY_SDCARD_ON_DATA := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
 TW_EXCLUDE_DEFAULT_USB_INIT := true
-TW_IGNORE_MISC_WIPE_DATA := true
+TW_EXCLUDE_SUPERSU := true
+TW_EXTRA_LANGUAGES := true
 TW_INCLUDE_NTFS_3G := true
-TW_INCLUDE_REPACKTOOLS := true
-TW_INPUT_BLACKLIST := hbtp_vm
-TW_INTERNAL_STORAGE_MOUNT_POINT := sdcard
-TW_INTERNAL_STORAGE_PATH := /data/media/0
+AB_OTA_UPDATER := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_MAX_BRIGHTNESS := 255
-TW_SCREEN_BLANK_ON_BOOT := true
+TW_DEFAULT_BRIGHTNESS := 130
 TW_THEME := portrait_hdpi
+TARGET_RECOVERY_DEVICE_MODULES += android.hardware.boot@1.0 qseecomd
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.0/lun.%d/file
+TARGET_RECOVERY_PIXEL_FORMAT := BGRA_8888
+TW_NO_SCREEN_BLANK := true
 TW_USE_TOOLBOX := true
-TW_EXCLUDE_TWRPAPP := true
-TW_Y_OFFSET := 97
-TW_H_OFFSET := -97
 
-# Debug flags
+# Encryption
+#PLATFORM_SECURITY_PATCH := 2019-05-01
+#TW_INCLUDE_CRYPTO := true
+#TARGET_HW_DISK_ENCRYPTION := true
+#TARGET_KEYMASTER_WAIT_FOR_QSEE := true
+#TARGET_CRYPTFS_HW_PATH := vendor/qcom/opensource/commonsys/cryptfs_hw
+#TW_CRYPTO_USE_SYSTEM_VOLD := qseecomd ven_keymaster-3-0-qti
+#TW_CRYPTO_SYSTEM_VOLD_MOUNT := vendor firmware
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
 
-# Workaround for error copying vendor files to recovery ramdisk
-TARGET_COPY_OUT_VENDOR := system/vendor
+# Extras
+TW_USE_LEDS_HAPTICS := true
+USE_RECOVERY_INSTALLER := true
+RECOVERY_INSTALLER_PATH := $(DEVICE_PATH)/installer
+TW_EXCLUDE_TWRPAPP := true
+TW_INCLUDE_REPACKTOOLS := true
+TW_Y_OFFSET := 97
+TW_H_OFFSET := -97
